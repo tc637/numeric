1c029fc29c7da1051a84370f636c3383b1d07369
Susan Allen <sallen@eos.ubc.ca>
2015-10-25 11:48:11 -0700

    Add small option.  Add variable depth.  Now one code "does it all"

--- numlabs/lab7/interactive1.py
+++ numlabs/lab7/interactive1.py
@@ -1,42 +1,54 @@
 #!/usr/bin/env python
 """Interactive One.  Initialize a Gaussian bump in surface height and allow it evolve.  Note that under
-rotational dynamics, the final steady state would be flow around the bump."""
+rotational dynamics, the final steady state would be flow around the bump. If you chose the smaller domain size
+even though the Coriolis force is still included, the scale is so small, its effect is negligible"""
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-def interactive1(grid, ngrid, dt, T):  # return eta
-
+def interactive1(grid, ngrid, dt, T, small=False):  # return eta
+    '''recommended values ngrid=11, dt=150, T=4*3600 (4 hours), small=False OR
+    ngrid=11, dt=4, T=90, small=True
+    '''
+    
     stepper = {1: stepgrid1, 2: stepgrid2, 3: stepgrid3}
-
-# recommended values ngrid=11, dt=150, T=4*3600 (4 hours)
+    find_depth = {1: find_depth1, 2: find_depth2, 3: find_depth3}
 
 # define rotation speed, gravity and depth
     f = 1e-4
     g = 10
-    H = 1000
+    H0 = 1000
 
 # set up spatial scale L is total domain size
     L = 1000e3
+
+# make the domain depth and width smaller if small is True
+    if small:
+        H0 = 10
+        L = 1000
+        
     dx = L/(ngrid-1)
 
 # write out dx/dt and sqrt(g*H) for comparison
     print('dx/dt {0:.3f}\n'.format(dx/dt))
-    print('sqrt(g*H) {0:.3f}\n'.format(np.sqrt(g*H)))
+    print('sqrt(g*H0) {0:.3f}\n'.format(np.sqrt(g*H0)))
     rdx = 1./dx
 
 # set up Gaussian for forcing (its a bump in the center of the domain)
     x, y, spatial = findforcing(L, dx, ngrid)
 
-    # set up temporal scale T is total run time
+# set up temporal scale T is total run time
     ntime = np.int(T/dt)
 
-    # initialize
+# initialize
     u, v, eta, up, vp, etap = initial(ngrid)
+
+# set-up topography
+    Hu, Hv = find_depth[grid](H0, ngrid)
 
     # main loop (leap-frog)
     for k in range(ntime):
-        u, v, eta = stepper[grid](ngrid, f, g, H, dt, rdx, u, v, eta, up, vp, etap)
+        u, v, eta = stepper[grid](ngrid, f, g, Hu, Hv, dt, rdx, u, v, eta, up, vp, etap)
 
         # add forcing
         t = k * dt / 8640
@@ -66,6 +78,24 @@ def findforcing(L, dx, ngrid):
                                     /(L*L / 48.))
     return x, y, spatial
 
+def find_depth1(H0, ngrid):
+    print ('Flat Bottom')
+    Hu = H0*np.ones((ngrid, ngrid))
+    Hv = H0*np.ones_like(Hu)
+    return Hu, Hv
+
+def find_depth2(H0, ngrid):
+    print ('Flat Bottom')
+    Hu = H0*np.ones((ngrid, ngrid))
+    Hv = H0*np.ones_like(Hu)
+    return Hu, Hv
+
+def find_depth3(H0, ngrid):
+    print ('Flat Bottom')
+    Hu = H0*np.ones((ngrid, ngrid))
+    Hv = H0*np.ones_like(Hu)
+    return Hu, Hv
+
 def initial(ngrid):
     '''initialize a ngrid x ngrid domain, u, v, and eta, all zero'''
 
@@ -79,7 +109,7 @@ def initial(ngrid):
 
     return u, v, eta, up, vp, etap
 
-def stepgrid1(ngrid, f, g, H, dt, rdx, u, v, eta, up, vp, etap):
+def stepgrid1(ngrid, f, g, Hu, Hv, dt, rdx, u, v, eta, up, vp, etap):
     '''take a step forward using grid 1 (A)'''
     n = ngrid
     nm1 = ngrid-1
@@ -88,11 +118,13 @@ def stepgrid1(ngrid, f, g, H, dt, rdx, u, v, eta, up, vp, etap):
                                                 - g * (etap[2:n, 1:nm1] - etap[0:nm2, 1:nm1]) * 0.5 * rdx)
     v[1:nm1, 1:nm1] = v[1:nm1, 1:nm1] + 2*dt * (-f * up[1:nm1, 1:nm1]
                                                 - g *(etap[1:nm1,2:n] - etap[1:nm1, 0:nm2]) * 0.5 * rdx)
-    eta[1:nm1, 1:nm1] = eta[1:nm1, 1:nm1] + 2*dt * (-H * (up[2:n, 1:nm1] - up[0:nm2, 1:nm1]) * 0.5 * rdx
-                                                    -H * (vp[1:nm1, 2:n] - vp[1:nm1, 0:nm2]) * 0.5 * rdx)
+    eta[1:nm1, 1:nm1] = eta[1:nm1, 1:nm1] + 2*dt * (-(Hu[2:n, 1:nm1] * up[2:n, 1:nm1]
+                                                      - Hu[0:nm2, 1:nm1] * up[0:nm2, 1:nm1]) * 0.5 * rdx
+                                                    -(Hv[1:nm1, 2:n] * vp[1:nm1, 2:n]
+                                                      - Hv[1:nm1, 0:nm2] * vp[1:nm1, 0:nm2]) * 0.5 * rdx)
     return u, v, eta
 
-def stepgrid2(ngrid, f, g, H, dt, rdx, u, v, eta, up, vp, etap):
+def stepgrid2(ngrid, f, g, Hu, Hv, dt, rdx, u, v, eta, up, vp, etap):
     '''take a step forward using grid 2 (B)'''
     n = ngrid
     nm1 = ngrid-1
@@ -103,13 +135,17 @@ def stepgrid2(ngrid, f, g, H, dt, rdx, u, v, eta, up, vp, etap):
     v[1:nm1, 1:nm1] = v[1:nm1, 1:nm1] + 2*dt * (-f*up[1:nm1, 1:nm1]
                                                  - g * (etap[1:nm1, 2:n] - etap[1:nm1, 1:nm1]
                                                       + etap[2:n, 2:n] - etap[2:n, 1:nm1]) * 0.5 * rdx)
-    eta[1:nm1, 1:nm1] = eta[1:nm1, 1:nm1] + 2*dt * (-H*(up[1:nm1, 1:nm1] - up[0:nm2, 1:nm1]
-                                                         + up[1:nm1, 0:nm2] - up[0:nm2, 0:nm2]
-                                                         + vp[1:nm1, 1:nm1] - vp[1:nm1, 0:nm2]
-                                                         +vp[0:nm2, 1:nm1] - vp[0:nm2, 0:nm2]) * 0.5 * rdx)
+    eta[1:nm1, 1:nm1] = eta[1:nm1, 1:nm1] + 2*dt * (-(Hu[1:nm1, 1:nm1] * up[1:nm1, 1:nm1]
+                                                      - Hu[0:nm2, 1:nm1] * up[0:nm2, 1:nm1]
+                                                         + Hu[1:nm1, 0:nm2] * up[1:nm1, 0:nm2]
+                                                         - Hu[0:nm2, 0:nm2] * up[0:nm2, 0:nm2]
+                                                         + Hv[1:nm1, 1:nm1] * vp[1:nm1, 1:nm1]
+                                                         - Hv[1:nm1, 0:nm2] * vp[1:nm1, 0:nm2]
+                                                         + Hv[0:nm2, 1:nm1] * vp[0:nm2, 1:nm1]
+                                                         - Hv[0:nm2, 0:nm2] * vp[0:nm2, 0:nm2]) * 0.5 * rdx)
     return u, v, eta
 
-def stepgrid3(ngrid, f, g, H, dt, rdx, u, v, eta, up, vp, etap):
+def stepgrid3(ngrid, f, g, Hu, Hv, dt, rdx, u, v, eta, up, vp, etap):
     '''take a step forward using grid 3 (C)'''
     n = ngrid
     nm1 = ngrid-1
@@ -121,8 +157,10 @@ def stepgrid3(ngrid, f, g, H, dt, rdx, u, v, eta, up, vp, etap):
                                                 - g * (etap[2:n, 1:nm1] - etap[1:nm1, 1:nm1]) * rdx)
     v[1:nm1, 1:nm1] = v[1:nm1, 1:nm1] + 2*dt * (-f * umid[1:nm1, 1:nm1]
                                                 - g *(etap[1:nm1, 2:n] - etap[1:nm1, 1:nm1]) * rdx)
-    eta[1:nm1, 1:nm1] = eta[1:nm1, 1:nm1] + 2*dt * (-H * (up[1:nm1, 1:nm1] - up[0:nm2, 1:nm1]) * rdx
-                                                    -H * (vp[1:nm1, 1:nm1] - vp[1:nm1, 0:nm2])* rdx)
+    eta[1:nm1, 1:nm1] = eta[1:nm1, 1:nm1] + 2*dt * (-(Hu[1:nm1, 1:nm1] * up[1:nm1, 1:nm1]
+                                                      - Hu[0:nm2, 1:nm1] * up[0:nm2, 1:nm1]) * rdx
+                                                    -(Hv[1:nm1, 1:nm1] * vp[1:nm1, 1:nm1]
+                                                      - Hv[1:nm1, 0:nm2] * vp[1:nm1, 0:nm2])* rdx)
     return u, v, eta
 
 def periodicbc(ngrid, u, v, eta):
